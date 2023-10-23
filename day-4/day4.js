0fe2c34fd75708648a29c3dbab35b9e8abf75804
Vamshix57 <vamshi.krishna@kalvium.community>
2023-10-23 12:13:50 +0530

    Done

---
+++ day-4/day4.js
@@ -0,0 +1,13 @@
+//Arthemaitic operators
+// console.log(1+2)
+// console.log(15-10)
+// console.log(10/2)
+// console.log(5*5)
+
+// console.log("Reamainder operator " + 11%2)
+
+//Increament and decreament operators
+var num = 15;
+var newNum = num++;
+console.log(num);
+console.log(num++);
