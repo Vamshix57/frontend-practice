4b5e90f58004820fe96d492d21bb461b281c506a
Vamshix57 <vamshi.krishna@kalvium.community>
2023-10-24 18:18:02 +0530

    Done

--- day-4/day4.js
+++ day-4/day4.js
@@ -32,10 +32,56 @@
 // console.log(num);
 // console.log(newNum);
 
-var num = 15;
-var newNum = num++ + 5;
-console.log(num);
-console.log(newNum);
+// var num = 15;
+// var newNum = num++ + 5;
+// console.log(num);
+// console.log(newNum);
+
+//Comparison operators
+
+var a = 30;
+var b = 10;
+
+//Equal (==)
+// console.log(a == b);
+
+//Not equal ( == )
+// console.log(a != b);
+
+//Greater than (>)
+// console.log(a>b);
+
+//Greater than or equal to (>=)
+// console.log(a >= b);
+
+//Less than (<)
+// console.log(a < b);
+
+//Less than or equal to (<=)
+// console.log(a <= b);
+
+//Logical operators
+var a = 30;
+var b = 20;
+
+//Logical AND (&&)
+
+// console.log(a>b && b>0);
+// console.log(a>b && b>-50 && b<0);
+
+//Logical OR (||)
+
+// console.log((a>b) || (b>0) || (b<0));
+
+//Logical Not(!)
+
+// console.log(!((a>0) || (b<0)));
+// console.log(!false);
+
+
+
+
+
 
 
 
